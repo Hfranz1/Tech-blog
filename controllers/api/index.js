203a54d3e625bf0421a2329bb17b07f61582bd86
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2022-02-22 11:30:26 -0700

    index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
-router.use('/blog', blogRoutes);
+router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
